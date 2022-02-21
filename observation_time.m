6a9c854c6c8acb06f9796aea7e370a4b1de9386a
brettcornman <100157789+brettcornman@users.noreply.github.com>
2022-02-21 13:31:42 -0500

    Add files via upload
    Assuming circular heliocentric orbit, given radius and inclination, this function finds the number of days within each period that the orbiting body is at or above a heliocentric latitude of 60 degrees

---
+++ observation_time.m
@@ -0,0 +1,25 @@
+function t_observation = observation_time(desired_radius, desired_inclination)
+
+% Constants
+mu = 1.327*10^11;    % Solar Gravitational Parameter [km^3/s^2]
+%%%%%
+
+
+period = 2*pi*(desired_radius^3/mu)^.5;   % [s]
+period = round(period);                   % Round Period to nearest second for use in forloop
+
+% Initialize Time of Observation
+t_observation = 0;
+%%%%%
+
+for time = 1:1:period
+    mean_anomaly = time*(mu / desired_radius^3)^.5 * 180 / pi;              % [deg]
+    r_z = desired_radius * sind(desired_inclination) * sind(mean_anomaly);  % Radius normal to sun's equatorial plane [km]
+    lat = asind(r_z / desired_radius);                                      % Heliocentric Latitude at time step [deg]      
+ 
+    if abs(lat) >= 60
+        t_observation = t_observation + 1;  % Increment time of observation
+    end
+end
+
+end
